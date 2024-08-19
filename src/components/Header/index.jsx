ec07b5b6960d32301d84f46db2ecf53de36a8720
Mezouar Mohamed <mohmezpro@etik.com>
2024-08-19 11:12:20 +0200

    fin chapitre 1

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
+import logo from '../../assets/dark-logo.png'
 
 const StyledLink = styled(Link)`
   padding: 15px;
@@ -11,16 +12,42 @@ const StyledLink = styled(Link)`
     props.$isFullLink &&
     `color: white; border-radius: 30px; background-color: ${colors.primary};`}
 `
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+`
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  img {
+    max-height: 80px;
+  }
+`
+
+const NavLinks = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 30px;
+`
 
 function Header() {
   return (
-    <nav>
-      <StyledLink to="/">Accueil</StyledLink>
-      <StyledLink to="/freelances">Profils</StyledLink>
-      <StyledLink to="/survey/1" $isFullLink>
-        Faire le test
-      </StyledLink>
-    </nav>
+    <HeaderContainer>
+        <LogoContainer>
+            <img src={logo} alt='logo shiny' />
+        </LogoContainer>
+        <NavLinks>
+        <StyledLink to="/">Accueil</StyledLink>
+        <StyledLink to="/freelances">Profils</StyledLink>
+        <StyledLink to="/survey/1" $isFullLink>
+            Faire le test
+        </StyledLink>
+        </NavLinks>
+    </HeaderContainer>
   )
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,60 @@
+import homeIllustration from '../../assets/home-illustration.svg'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+import { Link } from 'react-router-dom'
+
+const StyledLink = styled(Link)`
+  padding: 15px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+    display: inline-block;
+  margin-top: 20px;
+  padding: 15px 30px;
+  ${(props) =>
+    props.$isFullLink &&
+    `color: white; border-radius: 30px; background-color: ${colors.primary};`}
+`
+const HomeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding top: 60px;
+  background-color: #f9f9f9;
+  max-width: 90%;
+  margin: 0 auto;
+`
+const TextContainer = styled.div`
+  max-width: 500px;
+`
+const TextLine = styled.p`
+  font-family: 'Trebuchet MS', sans-serif;
+  font-size: 32px;
+  font-weight: 700;
+  margin: 5px 0;
+  color: #2f2e41;
+`
+ 
+const Illustration = styled.img`
+  max-width: 100%;
+  height: auto;
+`
+
 function Home() {
-  return <div>Page d'accueil 🏡</div>
+  return (
+    <HomeContainer>
+      <TextContainer>
+        <TextLine>Repérez vos besoins,</TextLine>
+        <TextLine>on s'occupe du reste,</TextLine>
+        <TextLine>avec les meilleurs</TextLine>
+        <TextLine>talents</TextLine>
+        <StyledLink to="/survey/1" $isFullLink>
+          Faire le test
+        </StyledLink>
+      </TextContainer>
+      <Illustration src={homeIllustration} alt='Illustration de la page daccueil' />
+    </HomeContainer>
+  )
 }
 
 export default Home
